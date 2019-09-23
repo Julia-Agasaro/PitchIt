136ee23dd318ee2e3201540d4238b7169a3c8b72
Julia Agasaro <agasarojulia@gmail.com>
2019-09-23 17:53:59 +0200

    Added profile function

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,10 @@
 from flask import render_template,request,redirect,url_for,abort, flash
 from . import main
 from flask_login import login_required, current_user
-from ..models import Pitch, User,Comment,Upvote,Downvote
-from .forms import PitchForm, CommentForm, UpvoteForm, DownvoteForm
+from ..models import Pitch, User,Comment,Upvote,Downvote,PhotoProfile
+from .forms import PitchForm, CommentForm, UpvoteForm, DownvoteForm,UpdateProfile
 from flask.views import View,MethodView
-from .. import db
+from .. import db,photos
 import markdown2
 
 
@@ -19,8 +19,8 @@ def index():
     title = 'Home'
     pickuplines = Pitch.query.filter_by(category="pickuplines")
     interviewpitch = Pitch.query.filter_by(category = "interviewpitch")
-    religiouspitch = Pitch.query.filter_by(category = "religiouspitch")
-    fashionpitch = Pitch.query.filter_by(category = "fashionpitch")
+    religiouspitch = Pitch.query.filter_by(category = "religious pitch")
+    fashionpitch = Pitch.query.filter_by(category = "fashion pitch")
 
     upvotes = Upvote.get_all_upvotes(pitch_id=Pitch.id)
     
@@ -65,14 +65,50 @@ def new_comment(pitch_id):
 
     all_comments = Comment.query.filter_by(pitch_id = pitch_id).all()
     return render_template('comments.html', form = form, comment = all_comments, pitch = pitch )
-    
-@main.route('/user/<uname>')
-def profile(uname):
-   user = User.query.filter_by(username = uname).first()
-   if user is None:
-       abort(404)
-   return render_template("profile/profile.html", user = user)
 
+@main.route('/user/<uname>')
+@login_required
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    get_pitches = Pitch.query.filter_by(user_id = current_user.id).all()
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user, description = get_pitches)
+
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
+
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        user_photo = PhotoProfile(pic_path = path,user = user)
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
 
 @main.route('/pitch/upvote/<int:pitch_id>/upvote', methods = ['GET', 'POST'])
 @login_required
