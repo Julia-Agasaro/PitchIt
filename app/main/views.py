58e2c3848abcaa8cd5142753479038e6d4a3ece0
Julia Agasaro <agasarojulia@gmail.com>
2019-09-25 10:12:57 +0200

    Imported Blog class

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,8 @@
 from flask import render_template,request,redirect,url_for,abort, flash
 from . import main
 from flask_login import login_required, current_user
-from ..models import Pitch, User,Comment,Upvote,Downvote,PhotoProfile
-from .forms import PitchForm, CommentForm, UpvoteForm, DownvoteForm,UpdateProfile
+from ..models import Blog, User,Comment,PhotoProfile
+
 from flask.views import View,MethodView
 from .. import db,photos
 import markdown2
