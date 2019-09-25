3430ab159000c52d9019feddae0fcb65ec34bcf6
Julia Agasaro <agasarojulia@gmail.com>
2019-09-25 10:16:17 +0200

    Imported Blog

--- manage.py
+++ manage.py
@@ -4,13 +4,12 @@ from app.models import *
 from  flask_migrate import Migrate, MigrateCommand
 # Creating app instance
 app = create_app('development')
-app = create_app('production')
 manager = Manager(app)
 manager.add_command('server',Server)
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
 @manager.shell
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Pitch = Pitch, Comment=Comment,Upvote=Upvote,Downvote=Downvote)
+    return dict(app=app, db=db, User=User, Blog= Blog, Comment=Comment)
 if __name__ == '__main__':
     manager.run()
